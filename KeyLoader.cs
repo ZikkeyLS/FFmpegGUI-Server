81a75036d8f3a34061cae88eb465d59e07de036b
ZikkeyLS <hisaha21@yandex.ru>
2023-01-28 22:03:23 +0300

    Initial commit

---
+++ KeyLoader.cs
@@ -0,0 +1,164 @@
+﻿using System.Text;
+
+namespace FFmpegGUI_Server
+{
+    internal class KeyLoader
+    {
+        private string _path;
+        private List<KeyStatus> _keys = new();
+
+        public DateTime AfterYear => DateTime.Now.AddYears(1);
+        public DateTime AfterMonth => DateTime.Now.AddMonths(1);
+        public DateTime AfterWeek => DateTime.Now.AddDays(7);
+
+        public KeyLoader(string keyPath = "Keys/db.json")
+        {
+            _path = keyPath;
+
+            if(!Directory.Exists(Path.GetDirectoryName(_path)))
+                Directory.CreateDirectory(Path.GetDirectoryName(_path));
+
+            if (!File.Exists(_path))
+                File.Create(_path).Close();
+
+            LoadKeys();
+
+            Thread thread = new Thread((e) => { FilterKeys(); });
+        }
+
+        private void FilterKeys()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000 * 60 * 60); // hour delay
+
+                lock (_keys)
+                {
+                    List<KeyStatus> finalKeys = new();
+
+                    _keys.ForEach((key) =>
+                    {
+                        if (DateTime.Now < key.expirationTime)
+                            finalKeys.Add(key);
+                    });
+
+                    _keys = finalKeys;
+                }
+            }
+        }
+
+        public bool TryRemoveKey(string key)
+        {
+            KeyStatus status = _keys.Find((keyStatus) => keyStatus.key == key);
+
+            if (status != null)
+            {
+                _keys.Remove(status);
+                SaveKeys();
+                return true;
+            }
+
+            return false;
+        }
+
+        public string TryAddKey(string key, DateTime expirationTime)
+        {
+            bool contains = ContainsKey(key);
+            if (!contains)
+                _keys.Add(new KeyStatus(key, expirationTime));
+            SaveKeys();
+
+            return contains ? "" : key;
+        }
+
+        public string AddKey(string key, DateTime expirationTime)
+        {
+            _keys.Add(new KeyStatus(key, expirationTime));
+            return key;
+        }
+
+        public string LinkedID(string key)
+        {
+            return _keys.Find((keyStatus) => keyStatus.key == key).userID;
+        }
+
+        public void LinkKey(string key, string userID)
+        {
+            _keys.Find((keyStatus) => keyStatus.key == key).userID = userID;
+            SaveKeys();
+        }
+
+        public string GenerateKey(DateTime expirationTime)
+        {
+            string key = GenerateKey();
+
+            while (ContainsKey(key))
+                key = GenerateKey();
+
+            AddKey(key.ToString(), expirationTime);
+            SaveKeys();
+
+            return key;
+        }
+
+        private string GenerateKey()
+        {
+            StringBuilder key = new StringBuilder();
+
+            for (int i = 0; i < 12; i++)
+            {
+                Random random = new Random();
+                random.Next(0, 9);
+
+                key.Append(random.Next(9));
+            }
+
+            return key.ToString();
+        }
+
+        public bool ContainsKey(string key)
+        {
+            bool ok = false;
+
+            _keys.ForEach((keyStatus) =>
+            {
+                if (keyStatus.key == key)
+                    ok = true;
+            });
+
+            return ok;
+        }
+
+        public void Clear()
+        {
+            _keys.Clear();
+            SaveKeys();
+        }
+
+        private void SaveKeys()
+        {
+            File.WriteAllText(_path, Newtonsoft.Json.JsonConvert.SerializeObject(_keys));
+        }
+
+        private void LoadKeys()
+        {
+            _keys = Newtonsoft.Json.JsonConvert.DeserializeObject<List<KeyStatus>>(File.ReadAllText(_path));
+            if (_keys == null)
+                _keys = new();
+        }
+    }
+
+    [Serializable]
+    internal class KeyStatus
+    {
+        public string key = "";
+        public string userID = "";
+        public DateTime expirationTime;
+
+        public KeyStatus(string key, DateTime expiration)
+        {
+            this.key = key;
+            this.expirationTime = expiration;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using FFmpegGUI_Server;
+using Riptide;
+using Riptide.Utils;
+
+internal class Program
+{
+    private static Server _server;
+    private static KeyLoader _keys;
+    private static bool _running;
+
+    private static void Main()
+    {
+        Console.Title = "FFmpegGUI-Server";
+        Console.WriteLine("Write EXIT to stop the program.");
+
+        RiptideLogger.Initialize(Console.WriteLine, true);
+        _running = true;
+
+        _keys = new KeyLoader();
+        new Thread(new ThreadStart(Loop)).Start();
+
+        while (_running)
+        {
+            string command = Console.ReadLine();
+            string[] commandParts = command.Split(' ');
+
+            if (command.ToLower() == "exit")
+                _running = false;
+            else if (commandParts.Length > 1 && commandParts[0] == "generate")
+            {
+                string key = "";
+
+                if (commandParts[1] == "y")
+                    key = _keys.GenerateKey(_keys.AfterYear);
+                else if (commandParts[1] == "m")
+                    key = _keys.GenerateKey(_keys.AfterMonth);
+                else if (commandParts[1] == "w")
+                    key = _keys.GenerateKey(_keys.AfterWeek);
+
+                if (key != "")
+                    Console.WriteLine("Generated key is: " + key);
+            }
+            else if (commandParts.Length > 2 && commandParts[0] == "add")
+            {
+                string key = "";
+
+                if (commandParts[2] == "y")
+                    key = _keys.TryAddKey(commandParts[1], _keys.AfterYear);
+                else if (commandParts[2] == "m")
+                    key = _keys.TryAddKey(commandParts[1], _keys.AfterMonth);
+                else if (commandParts[2] == "w")
+                    key = _keys.TryAddKey(commandParts[1], _keys.AfterWeek);
+
+                if (key != "")
+                    Console.WriteLine("Added key is: " + key);
+            }
+            else if (commandParts.Length > 1 && commandParts[0] == "remove")
+            {
+                if (_keys.TryRemoveKey(commandParts[1]))
+                    Console.WriteLine("Deleted key is: " + commandParts[1]);
+            }
+            else if (command.ToLower() == "clear")
+            {
+                _keys.Clear();
+            }
+            else
+                Console.WriteLine("Unknown command. Known commands:\n" +
+                    "EXIT - exit application\n" +
+                    "generate [y/m/w] - generate key for year, month or week\n" +
+                    "add [id] [y/m/w ]- add certain key for year, month or week\n" + 
+                    "remove [id] - delete key from db\n" +
+                    "clear - remove all keys from db");
+        }
+
+        Console.WriteLine("Exited successfully.");
+        Console.ReadLine();
+    }
+
+    private static void Loop()
+    {
+        _server = new Server
+        {
+            TimeoutTime = ushort.MaxValue // Max value timeout to avoid getting timed out for as long as possible when testing with very high loss rates (if all heartbeat messages are lost during this period of time, it will trigger a disconnection)
+        };
+        _server.Start(7777, 10);
+
+        while (_running)
+        {
+            _server.Update();
+            Thread.Sleep(10);
+        }
+
+        _server.Stop();
+    }
+
+
+    [MessageHandler((ushort)MessageId.RequestAuthorization)]
+    private static void RequestAuthorization(ushort fromClientId, Message message)
+    {
+        string userID = message.GetString();
+        string key = message.GetString();
+
+        ushort status = 3;
+
+        // also check is used, then link user to server.
+        if (_keys.ContainsKey(key))
+        {
+            string id = _keys.LinkedID(key);
+
+            if (id == userID)
+            {
+                status = 0;
+            }
+            else if(id != "")
+            {
+                status = 2;
+            }
+            else
+            {
+                status = 0;
+                _keys.LinkKey(key, userID);
+            }
+        }
+        else
+            status = 1;
+
+        ResponseAuthorization(fromClientId, status);
+    }
+
+    private static void ResponseAuthorization(ushort fromClientId, ushort status)
+    {
+        Message message = Message.Create(MessageSendMode.Reliable, MessageId.ResponseAuthorization);
+        message.AddUShort(status);
+        // message.AddLong(keyLifetime)
+
+        _server.Send(message, fromClientId);
+    }
+}
+
+public enum ConnectionStatus : ushort
+{
+    Connected = 0,
+    WrongKey = 1,
+    AlreadyUsed = 2,
+    ErrorOnServer = 3
+}
+
+public enum MessageId : ushort
+{
+    RequestAuthorization = 0,
+    ResponseAuthorization
+}
